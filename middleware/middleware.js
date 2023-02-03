b4e86fbd7e176c14cf1d1bd245060e3f6fc4cd0e
Ahmad Bader <ahmad.bader@gunsel.local>
2023-02-03 11:18:01 +0200

    refactor auth()

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -4,22 +4,26 @@ const config = process.env;
 
 const auth = (req, res, next) => 
 {
-    const {token} = req.body;
+    const token = req.headers.token
+    
     if (!token) {
         return res.status(403).send("A token is required for authentication");
     }
 
     try 
     {
-        jwt.verify(token, config.TOKEN_KEY, (err, user) => {
-            if(err) return;
-            req.user = user
-            next()
-        });
+        jwt.verify(token, config.TOKEN_KEY, 
+            (err, user) => 
+            {
+                if(err) return;
+                req.user = user
+                next()
+            }
+        );
 
     } catch (err) 
     {
-        return res.status(401).send("Invalid Token");
+        res.status(401).send("Invalid Token");
     }
 };
 
